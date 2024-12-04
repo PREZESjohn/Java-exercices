bb2f1d6efe092f46a1e35996685fa9ac95c796f6
PREZESjohn <103060257+PREZESjohn@users.noreply.github.com>
2024-12-04 18:12:27 +0100

    initial commit

---
+++ src/main/java/org/example/ArrListLen/Solution.java
@@ -0,0 +1,10 @@
+package org.example.ArrListLen;
+
+public class Solution {
+    public int solution(int[] A){
+        return 1;
+    }
+    public void test(){
+        System.out.println("Test");
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+import org.example.ArrListLen.*;
+
+public class Main {
+    public static void main(String[] args) {
+        var klasa = new Solution();
+        klasa.test();
+    }
+}
