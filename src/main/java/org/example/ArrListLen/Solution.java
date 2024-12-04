41be46a98cd2ae859cd5274fc575b1bdcba6b57b
PREZESjohn <103060257+PREZESjohn@users.noreply.github.com>
2024-12-04 18:49:46 +0100

    ArrListLen task

--- src/main/java/org/example/ArrListLen/Solution.java
+++ src/main/java/org/example/ArrListLen/Solution.java
@@ -2,7 +2,17 @@ package org.example.ArrListLen;
 
 public class Solution {
     public int solution(int[] A){
-        return 1;
+        int counter=1;
+        int pos=0;
+        while(1==1){
+            if(A[pos]!=-1){
+                pos=A[pos];
+                counter++;
+            }else{
+                break;
+            }
+        }
+        return counter;
     }
     public void test(){
         System.out.println("Test");
