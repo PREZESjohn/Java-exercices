0e1d740d9a1c99f68fadb0bbb2491325c3f3bddd
PREZESjohn <103060257+PREZESjohn@users.noreply.github.com>
2024-12-05 18:17:54 +0100

    different approach for task CountBoundedSlices

--- src/main/java/org/example/CountBoundedSlices/Solution.java
+++ src/main/java/org/example/CountBoundedSlices/Solution.java
@@ -1,20 +1,31 @@
 package org.example.CountBoundedSlices;
 
 import java.util.Arrays;
+import java.util.TreeSet;
 
 public class Solution {
     public int solution(int K, int[] A) {
-        int[] tmp;
-        int counter=0;
-        int min, max;
-        for(int i=0; i<A.length; i++){
-            for(int j=i; j<A.length; j++){
-                tmp=Arrays.copyOfRange(A,i,j+1);
-                if((Arrays.stream(tmp).max().getAsInt()-Arrays.stream(tmp).min().getAsInt())<=K){
-                    counter++;
-                }
+        int n = A.length;
+        int count = 0;
+        int left = 0;
+
+        TreeSet<Integer> sliceElements = new TreeSet<>();
+
+        for (int right = 0; right < n; right++) {
+            sliceElements.add(A[right]);
+
+            while (sliceElements.last() - sliceElements.first() > K) {
+                sliceElements.remove(A[left]);
+                left++;
+            }
+
+            count += (right - left + 1);
+
+            if (count > 1_000_000_000) {
+                return 1_000_000_000;
             }
         }
-        return counter;
+
+        return count;
     }
 }
