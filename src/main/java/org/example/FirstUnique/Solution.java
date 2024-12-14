2d457862d6df06f999a33a97078256aee3af647c
PREZESjohn <103060257+PREZESjohn@users.noreply.github.com>
2024-12-14 12:32:29 +0100

    v2 of FirstUnique

--- src/main/java/org/example/FirstUnique/Solution.java
+++ src/main/java/org/example/FirstUnique/Solution.java
@@ -1,12 +1,13 @@
 package org.example.FirstUnique;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.function.BiFunction;
 
 public class Solution {
     public int solution(int A[]){
-        Map<Integer, Integer> baza = new HashMap<>();
+        Map<Integer, Integer> baza = new LinkedHashMap<>();
         BiFunction<Integer, Integer, Integer> addFunction = Integer::sum;
         for (int j : A) {
             baza.merge(j, 1, (k, v) -> addFunction.apply(v, 1));
