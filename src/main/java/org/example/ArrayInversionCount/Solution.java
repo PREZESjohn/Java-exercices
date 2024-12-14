0b73ad38b9144ad2fd5f333815cb8a7e6a4c0a54
PREZESjohn <103060257+PREZESjohn@users.noreply.github.com>
2024-12-14 13:08:20 +0100

    v2 of ArrayInversionCount (even worse efficiency)

--- src/main/java/org/example/ArrayInversionCount/Solution.java
+++ src/main/java/org/example/ArrayInversionCount/Solution.java
@@ -1,14 +1,15 @@
 package org.example.ArrayInversionCount;
 
+import java.util.stream.IntStream;
+
 public class Solution {
     public int solution(int[] A){
-        int result=0;
-        for (int i = 0; i < A.length; i++) {
-            for (int j = i+1; j <A.length; j++) {
-                if(A[j]<A[i]) result++;
-                if(result>1000000000) return -1;
-            }
-        }
+        int result= (int) IntStream.range(0, A.length)
+                .boxed()
+                .flatMap(i -> IntStream.range(i + 1, A.length)
+                        .filter(j -> A[i] > A[j])
+                        .mapToObj(j -> new int[]{i, j}))
+                .count();
         return result;
     }
 }
