45105ef70e3bb391776b33dc308f529da2081214
PREZESjohn <103060257+PREZESjohn@users.noreply.github.com>
2024-12-14 13:33:05 +0100

    v3 of ArrayInversionCount

--- src/main/java/org/example/ArrayInversionCount/Solution.java
+++ src/main/java/org/example/ArrayInversionCount/Solution.java
@@ -3,13 +3,57 @@ package org.example.ArrayInversionCount;
 import java.util.stream.IntStream;
 
 public class Solution {
-    public int solution(int[] A){
-        int result= (int) IntStream.range(0, A.length)
-                .boxed()
-                .flatMap(i -> IntStream.range(i + 1, A.length)
-                        .filter(j -> A[i] > A[j])
-                        .mapToObj(j -> new int[]{i, j}))
-                .count();
-        return result;
+    public int solution(int[] A) {
+        int[] temp = new int[A.length];
+        long inversionCount = mergeSortAndCount(A, temp, 0, A.length - 1);
+        return inversionCount > 1_000_000_000 ? -1 : (int) inversionCount;
+    }
+
+    private long mergeSortAndCount(int[] A, int[] temp, int left, int right) {
+        if (left >= right) {
+            return 0;
+        }
+
+        int mid = (left + right) / 2;
+        long inversions = 0;
+
+        inversions += mergeSortAndCount(A, temp, left, mid);
+        inversions += mergeSortAndCount(A, temp, mid + 1, right);
+
+        inversions += mergeAndCount(A, temp, left, mid, right);
+
+        return inversions;
+    }
+
+    private long mergeAndCount(int[] A, int[] temp, int left, int mid, int right) {
+        int i = left;
+        int j = mid + 1;
+        int k = left;
+        long inversions = 0;
+
+        while (i <= mid && j <= right) {
+            if (A[i] <= A[j]) {
+                temp[k++] = A[i++];
+            } else {
+                temp[k++] = A[j++];
+                inversions += (mid - i + 1);
+                if (inversions > 1_000_000_000) {
+                    return inversions;
+                }
+            }
+        }
+
+        while (i <= mid) {
+            temp[k++] = A[i++];
+        }
+        while (j <= right) {
+            temp[k++] = A[j++];
+        }
+
+        for (i = left; i <= right; i++) {
+            A[i] = temp[i];
+        }
+
+        return inversions;
     }
 }
