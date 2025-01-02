9610e9c04b9bfbbd8e0b52c67f31aba5a55ab5f4
PREZESjohn <103060257+PREZESjohn@users.noreply.github.com>
2025-01-02 15:59:07 +0100

    v1 of DisappearingPairs task

--- src/main/java/org/example/DisappearningPairs/Solution.java
+++ src/main/java/org/example/DisappearningPairs/Solution.java
@@ -2,6 +2,17 @@ package org.example.DisappearningPairs;
 
 public class Solution {
     public String solution(String S) {
-        return "test";
+        return findPair(S);
+    }
+    
+    private String findPair(String s){
+        String result;
+        for (int i = 0; i < s.length()-1; i++) {
+            if(s.substring(i, i + 2).equals("AA")||s.substring(i, i + 2).equals("BB")||s.substring(i, i + 2).equals("CC")){
+                result=s.substring(i,i+1);
+                findPair(result);
+            }
+        }
+        return s;
     }
 }
