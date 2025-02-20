f4f34c25cfc5e0c31bd8d3663fed4a74d595436e
PREZESjohn <103060257+PREZESjohn@users.noreply.github.com>
2025-02-20 17:26:30 +0100

    Isograms v2

--- src/main/java/org/example/Isograms/Solution.java
+++ src/main/java/org/example/Isograms/Solution.java
@@ -8,14 +8,7 @@ Assume the empty string is an isogram. Ignore letter case.
 
 public class Solution {
     public boolean solution(String S) {
-        S=S.toLowerCase();
-        for (int i = 0; i < S.length(); i++) {
-            for (int j = i+1; j < S.length(); j++) {
-                if (S.charAt(i) == S.charAt(j)) {
-                    return false;
-                }
-            }
-        }
-        return  true;
+
+        return  S.length()==S.toLowerCase().chars().distinct().count();
     }
 }
