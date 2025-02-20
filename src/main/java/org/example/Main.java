4ce4594417e523176be18ddbe4b70f5ce1721f8b
PREZESjohn <103060257+PREZESjohn@users.noreply.github.com>
2025-02-20 17:02:15 +0100

    new task - Multiples of 3 or 5

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,11 @@
 package org.example;
-import org.example.SockLaundering.*;
+import org.example.MultiplesOneOrFive.*;
 
 public class Main {
     public static void main(String[] args) {
         var klasa = new Solution();
         int[] c = {1,2,1,1};
         int[] d = {1,4,3,2,4};
-        System.out.println(klasa.solution(2,c,d));
+        System.out.println(klasa.solution(20));
     }
 }
